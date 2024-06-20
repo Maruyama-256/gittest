0e828fd50c3a003e5ca27d27bb3999d31bfdb1e8
Kouhei Maruyama <k.maruyama@softtex.co.jp>
2024-06-20 14:04:47 +0900

    新規リポジトリの作成

---
+++ test-git/src/test/Main.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Talk talk = new Talk();
+		
+		talk.tell("こんばんは");
+	}
+
+}
---
+++ test-git/src/test/Talk.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Talk {
+	
+	public String tell(String tell)
+	{
+		System.out.println(tell);
+		return tell;
+	}
+
+}
